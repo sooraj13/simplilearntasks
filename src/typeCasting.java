e75de098ded83db51b0026e8fb6657a1ca9c69c5
Sooraj PV <soorajpv4u@gmail.com>
2021-03-13 16:24:44 +0000

    first commit

---
+++ src/typeCasting.java
@@ -0,0 +1,21 @@
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		char a = 'z';
+		int b = a;
+		System.out.println("ascii value of b : " + b);
+		
+		float d = 2.22f;
+		int f = (int) d;
+		System.out.println("explicit conv f " + f);
+		
+		
+
+		
+	}
+
+}
